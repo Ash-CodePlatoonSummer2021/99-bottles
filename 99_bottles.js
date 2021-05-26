2ea69a6b8f54a94bb3ca94a35cf80b01bd91a931
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-26 15:15:31 -0400

    python version

--- 99_bottles.js
+++ 99_bottles.js
@@ -7,8 +7,11 @@ function bottleSong(num) {
   if(num==0){
     console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall");
   }
-  console.log(num+" bottles of beer on the wall, "+num+" bottles of beer. Take one down and pass it around, "+num-1+" bottles of beer on the wall.")
-  bottleSong(num-1);
+  else{
+    console.log(num+" bottles of beer on the wall, "+num+" bottles of beer. Take one down and pass it around, "+num-1+" bottles of beer on the wall.")
+    bottleSong(num-1);
+  }
+ 
 };
 
 bottleSong(99);
--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,12 @@
-def bottle_song():
+def bottle_song(num):
 	# write your code here!
+	if(num==1):
+		print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
+		bottle_song(0)
+	elif(num==0):
+		print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall")
+	else:
+		print(str(num)+" bottles of beer on the wall, "+str(num)+" bottles of beer. Take one down and pass it around, "+str(num-1)+" bottles of beer on the wall.")
+		bottle_song(num-1)
 
-
-bottle_song()
+bottle_song(99)
